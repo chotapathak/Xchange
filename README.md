009c22c1b00a940ae89c4e47d1dc2afc12e9f368
Vikash Pathak <48418358+chotapathak@users.noreply.github.com>
2021-12-25 14:44:12 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# Learning Hand
+
+Blockchain basics Tasks . Learning how to build Decentralized
+Applications (Dapps) on Ethereum and Solidity Smart contracts
+
+Covers:
+* Solidity
+* Web3
+* DeFi (Decentralized Finance)
+
+# Just like uniswap  
+
+* Important
+
+Currently It's just shows how much DAI you get on swaping 1 ETH
+
+## Instalation
+
+```
+npm install @uniswap/sdk
+
+```
---
+++ index.js
@@ -0,0 +1,18 @@
+const { ChainId, Fetcher, WETH, Route, Trade, TokenAmount, TradeType } = require('@uniswap/sdk');
+
+const chainId = ChainId.MAINNET;
+const tokenAddress = '0x6b175474e89094c44da98b954eedeac495271d0f';
+
+const init = async () => {
+  const dai = await Fetcher.fetchTokenData(chainId, tokenAddress);
+  const weth = WETH[chainId];
+  const pair = await Fetcher.fetchPairData(dai, weth);
+  const route = new Route([pair], weth);
+  const trade = new Trade(route, new TokenAmount(weth,'100000000000000000'), TradeType.EXACT_INPUT);
+  console.log(route.midPrice.toSignificant(6));
+  console.log(route.midPrice.invert().toSignificant(6));
+  console.log(trade.executionPrice.toSignificant(6));
+  console.log(trade.nextMidPrice.toSignificant(6));
+}
+
+init();
